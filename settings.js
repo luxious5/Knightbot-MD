60591495984550567f5993ab712fecf9e2075e6c
luxious5 <bobbymwaura737@gmail.com>
2025-07-29 14:19:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'luxious5', // Your name
+  botOwner: 'luxious', // Your name
   ownerNumber: '+254795861698', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
